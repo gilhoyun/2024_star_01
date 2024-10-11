6fe4b9f796ba692761bcb8af3c68a50f840155fd
gilhoyun <gilhoyun04@gmail.com>
2024-10-11 17:34:49 +0900

    1번 제출

---
+++ src/com/koreaIT/BAM/Main.java
@@ -0,0 +1,24 @@
+package com.koreaIT.BAM;
+
+public class Main {
+	public static void main(String[] args) {
+		for (int i = 0; i < 4; i++) {  
+			  for (int j = 0; j < 3; j++) {  
+			      System.out.print("*"); 
+			  }
+			  System.out.println(); 
+			}
+		
+		
+		for (int i = 0; i < 2; i++) {  
+			  for (int j = 0; j < 4; j++) {  
+			      System.out.print("*"); 
+			  }
+			  System.out.println(); 
+			}
+		
+		
+	}
+}
+
+
